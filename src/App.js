7f3f3d7a2bdace90784fca39ea615dad383c28ce
Audrey Tan <audrey.wanmun.tan@gmail.com>
2023-11-04 18:27:19 -0500

    moved header to its own component

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
+function Header(){
+  return(
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer">
           Learn React
         </a>
-        <button>This is a button</button>
       </header>
+  )
+}
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <button>This is a button</button>
     </div>
   );
 }
