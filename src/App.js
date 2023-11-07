5dfbd9c7e9727378be9090a242a20e92f06ee345
Audrey Tan <audrey.wanmun.tan@gmail.com>
2023-11-06 20:37:31 -0600

    added dynamic object calls for phone number and email. Added comments explaining code changes

--- src/App.js
+++ src/App.js
@@ -1,18 +1,25 @@
 import logo from './logo.svg';
 import './App.css';
-
+export const audreyInfo = {
+    phone: 'Business Phone: 830-570-3628',
+    email: 'Business Email: audrey.wanmun.tan@gmail.com'
+}
 const audreyAdjectives = ['Hard-working', 'Fast-learning', 'Reliable', 'Respectable', 'Personable', 'Leadership-qualified'];
 function getRandomInt(max){
     return Math.floor(Math.random()* (max + 1));
 }
 function Header(){
+      const audreyFullStackAdj = audreyAdjectives[getRandomInt(5)];
   return(
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src= {logo} className="App-logo" alt="logo" />
           <h1> Audrey Tan:</h1>
         <p>
-            {audreyAdjectives[getRandomInt(5)]} Full-Stack Web Developer
+            {audreyFullStackAdj} Full-Stack Web Developer
         </p>
+          <h2> For business inquiries: </h2>
+          <h2> {audreyInfo.phone}</h2>
+          <h2> {audreyInfo.email}</h2>
         <a
             className="App-link"
             href="https://reactjs.org"
