67d7277b444a33724658729d2f0e42d27a88f2fa
Audrey Tan <audrey.wanmun.tan@gmail.com>
2023-11-06 20:37:47 -0600

    comments added

--- src/App.js
+++ src/App.js
@@ -1,16 +1,24 @@
 import logo from './logo.svg';
 import './App.css';
+// Using export object info into a React JSX code. Like calling object properties in JS
 export const audreyInfo = {
     phone: 'Business Phone: 830-570-3628',
     email: 'Business Email: audrey.wanmun.tan@gmail.com'
 }
+
+// Testing random dynamic value generator with array list of strings and Math.random method.
 const audreyAdjectives = ['Hard-working', 'Fast-learning', 'Reliable', 'Respectable', 'Personable', 'Leadership-qualified'];
 function getRandomInt(max){
     return Math.floor(Math.random()* (max + 1));
 }
+
+// React Header component
 function Header(){
+      // call for random string value in adjectives list , declare within scope of function
       const audreyFullStackAdj = audreyAdjectives[getRandomInt(5)];
+
   return(
+      // JSX code to build header
       <header className="App-header">
         <img src= {logo} className="App-logo" alt="logo" />
           <h1> Audrey Tan:</h1>
@@ -30,6 +38,8 @@ function Header(){
       </header>
   )
 }
+
+// App function to return JSX code to build the header and add a button after Header.
 function App() {
   return (
     <div className="App">
@@ -39,4 +49,5 @@ function App() {
   );
 }
 
+// Exports App function to any other file that imports the function.
 export default App;
